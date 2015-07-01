fdb71a1a864393399b24ef38932cf5fa62144b5e
Jeffrey Leonard <jeffrey9279@gmail.com>
2015-07-01 13:41:57 -0700

    two

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  post 'anagram/find'
+
+  get 'anagram/index'
+
+  root "anagram#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
