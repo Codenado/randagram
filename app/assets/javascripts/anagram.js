32dbdd6f2806cfde0e252d09b3ea0661a271217f
Jeffrey Leonard <jeffrey9279@gmail.com>
2015-07-02 23:38:43 -0700

    changed layout

--- app/assets/javascripts/anagram.js
+++ app/assets/javascripts/anagram.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
    $('form').on("ajax:success", function(e, data, status, xhr) {
    
-    $('ol').replaceWith(xhr.responseText)
+    $('.anagrams').replaceWith(xhr.responseText)
   }).on("ajax:error", function(e, xhr, status, error) {
 
   });
