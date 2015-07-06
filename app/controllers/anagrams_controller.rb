73d595dbe1430fbff2e0d3d849408631044eed09
Jeffrey Leonard <jeffrey9279@gmail.com>
2015-07-05 21:19:21 -0700

    version 1.0

--- app/controllers/anagrams_controller.rb
+++ app/controllers/anagrams_controller.rb
@@ -4,9 +4,11 @@ class AnagramsController < ApplicationController
   end	
 
   def find
-   @word = Word.find_by(text: params["term"]) 
-      @words = Word.where(anagram_key: @word.anagram_key)
-        render :words
+   	@word = Word.find_by(text: params["term"]) 
+   	if @word
+      @anagrams = Word.where(anagram_key: @word.anagram_key).where.not(text: @word.text)
+   	end
+    render partial: "anagrams"
   
   end
 
