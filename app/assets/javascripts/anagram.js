97baca0038a2118cc0ea5a026e93061c5f9c9f67
Jeffrey Leonard <jeffrey9279@gmail.com>
2015-07-08 22:55:14 -0700

    changed to tables

--- app/assets/javascripts/anagram.js
+++ app/assets/javascripts/anagram.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
    $('form').on("ajax:success", function(e, data, status, xhr) {
    
-    $(".anagrams ol").replaceWith(xhr.responseText)
+    $(".anagrams table").replaceWith(xhr.responseText)
   }).on("ajax:error", function(e, xhr, status, error) {
 
   });
