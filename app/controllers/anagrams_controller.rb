c4c362f67c03d22bb4a1d169931761fec4d14f39
Jeffrey Leonard <jeffrey9279@gmail.com>
2015-07-10 21:25:56 -0700

    added json

--- app/controllers/anagrams_controller.rb
+++ app/controllers/anagrams_controller.rb
@@ -5,11 +5,14 @@ class AnagramsController < ApplicationController
 
   def find
    	@word = Word.find_by(text: params["term"]) 
-   	if @word
-      @anagrams = Word.where(anagram_key: @word.anagram_key).where.not(text: @word.text)
-   	end
-    render partial: "anagrams"
-  
+   	respond_to do |format|
+	   	if @word
+	      @anagrams = Word.where(anagram_key: @word.anagram_key).where.not(text: @word.text)
+	   	end
+	    format.html { render partial: "anagrams" }
+  		format.json { render json: @anagrams }
+
+  	end
   end
 
   
