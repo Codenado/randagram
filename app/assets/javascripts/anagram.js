c689226e0101223116091bd3fad2b7167ef4b5d0
Jeffrey Leonard <jeffrey9279@gmail.com>
2015-07-11 22:29:46 -0700

    update

--- app/assets/javascripts/anagram.js
+++ app/assets/javascripts/anagram.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
    $('form').on("ajax:success", function(e, data, status, xhr) {
    
-    $(".anagrams table").replaceWith(xhr.responseText)
+    $(".anagrams ol").replaceWith(xhr.responseText)
   }).on("ajax:error", function(e, xhr, status, error) {
 
   });
